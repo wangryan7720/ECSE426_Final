773260fbd6a4032f5436941585d5983e4459989c
Ryan Wang <luoqing.wang@mail.mcgill.ca>
2017-11-27 17:33:09 -0500

    commit Discovery board

---
+++ Discovery/Final_Project/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Final_Project' 
+ * Target:  'Final_Project' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
